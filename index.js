b39bed04180ac70df6d42368144f4596fbf40c31
Kostiantyn Rukhlov <rukhlovka@gmail.com>
2022-09-28 16:40:13 +0300

    tiny js world – done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { print } from "./js/lib.js";
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-let dog = {
+const dog = {
    species: "dog",
    name: "Rick",
    gender: "male",
@@ -18,7 +18,7 @@ let dog = {
    saying: "woof-woof!",
    hair: "brown"
 };
-let cat = {
+const cat = {
    species: "cat",
    name: "Morty",
    gender: "female",
@@ -27,7 +27,7 @@ let cat = {
    saying: "mew-mew!",
    hair: "grey"
 };
-let man = {
+const man = {
    species: "human",
    name: "Greg",
    gender: "male",
@@ -36,7 +36,7 @@ let man = {
    saying: "Hello, world!",
    hair: "blond"
 };
-let woman = {
+const woman = {
    species: "human",
    name: "Greg",
    gender: "female",
@@ -45,7 +45,7 @@ let woman = {
    saying: "Hello, world!",
    hair: "red"
 };
-let catWomen = {
+const catWomen = {
    species: "human",
    name: "Belatris",
    gender: "female",
@@ -53,35 +53,20 @@ let catWomen = {
    hands: 2,
    saying: cat.saying,
    hair: "brown"
-
 };
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,"div") to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const list = [man, woman, cat, dog, catWomen]
 
-/* Print examples:
-   print("ABC");
-   print("<strong>ABC</strong>");
-   print("<strong>ABC</strong>", "div");
+function addInformation(somebody) {
+   let result = [];
 
-   print("human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny");
-   print("human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny");
-   print("human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny", "div");
-   */
+   for (let i in somebody) {
+      result.push(`${i}: ${somebody[i]};`);
+   }
 
+   return result.join(' ');
+}
 
-print(dog.species + ';' + dog.name + ';' + dog.gender + ';' + dog.legs + ';' + dog.hands + ';' + dog.saying, 'div');
-
-print(cat.species + ';' + cat.name + ';' + cat.gender + ';' + cat.legs + ';' + cat.hands + ';' + cat.saying, 'div');
-
-print(man.species + ';' + man.name + ';' + man.gender + ';' + man.legs + ';' + man.hands + ';' + man.saying, 'div');
-
-print(woman.species + ';' + woman.name + ';' + woman.gender + ';' + woman.legs + ';' + woman.hands + ';' + woman.saying, 'div');
-
-print(catWomen.species + ';' + catWomen.name + ';' + catWomen.gender + ';' + catWomen.legs + ';' + catWomen.hands + ';' + catWomen.saying, 'div');
+list.forEach(inhabitant => print(addInformation(inhabitant)))
