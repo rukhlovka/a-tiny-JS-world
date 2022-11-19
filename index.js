e6e17f4ab2794c8266f31aedc1f2029e16187d33
Kostiantyn Rukhlov <rukhlovka@gmail.com>
2022-11-20 01:21:55 +0200

    done oop task

--- index.js
+++ index.js
@@ -9,67 +9,85 @@ import { print } from "./js/lib.js";
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-const dog = {
-   species: "dog",
-   name: "Rick",
-   gender: "male",
-   legs: 4,
-   hands: 0,
-   saying: "woof-woof!",
-   hair: "brown"
+class Essence {
+   constructor(species, name, gender, legs, saying) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.legs = legs;
+      this.saying = saying;
+      this.attributes = [
+         { attribute: "species", text: "I am a" },
+         { attribute: "name", text: "My name is" },
+         { attribute: "gender", text: "My gender:" },
+         { attribute: "legs", text: "I have legs:" },
+         { attribute: "saying", text: "My favorite phrase: " },
+      ];
+   }
+   showEssenceAttributes() {
+      return this.attributes.map(({ attribute, text }) => `${text} ${this[attribute]}`).join("; ");
+   }
 };
-const cat = {
-   species: "cat",
-   name: "Morty",
-   gender: "female",
-   legs: 4,
-   hands: 0,
-   saying: "mew-mew!",
-   hair: "grey"
+
+class Human extends Essence {
+   constructor(name, gender, saying, hair) {
+      super("human", name, gender, 2, saying);
+      this.hands = 2;
+      this.hair = hair;
+      this.attributes = [
+         ...this.attributes,
+         { attribute: "hands", text: "I have hands:" },
+         { attribute: "hair", text: "My hair color is:" },
+      ];
+   }
 };
-const man = {
-   species: "human",
-   name: "Greg",
-   gender: "male",
-   legs: 2,
-   hands: 2,
-   saying: "Hello, world!",
-   hair: "blond"
+
+class Man extends Human {
+   constructor(name, saying, hair) {
+      super(name, "male", saying, hair);
+   }
 };
-const woman = {
-   species: "human",
-   name: "Greg",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: "Hello, world!",
-   hair: "red"
+
+class Woman extends Human {
+   constructor(name, saying, hair) {
+      super(name, "famale", saying, hair);
+   }
 };
-const catWomen = {
-   species: "superhuman",
-   name: "Belatris",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
-   hair: "brown"
+
+class Animal extends Essence {
+   constructor(species, name, gender, saying, fur) {
+      super(species, name, gender, 4, saying);
+      this.fur = fur;
+      this.attributes = [
+         ...this.attributes,
+         { attribute: "fur", text: "The color of my fur is:" },
+      ];
+   }
 };
-const inhabitantAttributes = [
-   'species', 'name', 'gender', 'legs', 'hands', 'saying', 'hair'
+class Cat extends Animal {
+   constructor(name, gender, fur) {
+      super("cat", name, gender, "Meaw-meaaaaw", fur);
+   }
+};
+class Dog extends Animal {
+   constructor(name, gender, fur) {
+      super("dog", name, gender, "Gav-gav", fur);
+   }
+};
+
+const entities = [
+   new Woman("Sara", "Hello!", "blond"),
+   new Woman("Lily", "hi", "red"),
+   new Woman("Sara", "Hello everybody", "brown"),
+   new Man("Simon", "Yo-yo", "silver"),
+   new Man("Bill", "I am top", "brown"),
+   new Man("Boris", "Hello everybody", "blond"),
+   new Cat("Tom", "female", "grey"),
+   new Cat("Jerry", "male", "white"),
+   new Dog("Pes Duck", "female", "black and white"),
+   new Dog("Bob", "male", "brown"),
 ];
 
-const inhabitantList = [man, woman, cat, dog, catWomen];
-
-// ======== OUTPUT ========
-
-function showInformation(somebody) {
-   let result = [];
-
-   inhabitantAttributes.map(attribute => {
-      result.push(somebody[attribute]);
-   });
-
-   return result.join('; ');
-}
-
-inhabitantList.forEach(inhabitant => print(showInformation(inhabitant)));
+entities.forEach((essence) => {
+   print(essence.showEssenceAttributes());
+});
